408cbb8b0b723c604299477a13103bb2107082c2
Cedric Kring <cedric.kring@gmail.com>
2021-07-31 20:22:45 +0200

    add contact me button

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,8 +1,9 @@
-import { GitHub, LinkedIn, Xing } from "./Icons";
+import { Email, GitHub, LinkedIn, Xing } from "./Icons";
 
 export default function Header() {
   return (
-    <div className="flex flex-row items-center h-14 pl-6 select-none">
+    <div className="flex flex-row items-center h-14 px-6 select-none justify-between">
+      <div className="flex flex-row">
       <h1 className="text-l font-bold text-gray-200">Cedric Kring</h1>
       <div className="h-6 border-r-2 border-solid border-gray-200 ml-6" />
       <a
@@ -28,6 +29,15 @@ export default function Header() {
       >
         <Xing className="inline mr-2 h-4 w-4" />
         Xing
+      </a>
+      </div>
+      <a
+        target="_blank"
+        href="mailto:cedric.kring@gmail.com"
+        className="flex flex-row items-center text-indigo-900 font-semibold bg-gray-100 hover:bg-white hover:text-indigo-800 rounded px-2 py-1.5"
+      >
+        <Email className="inline mr-2 h-5 w-5" />
+        Contact me
       </a>
     </div>
   );

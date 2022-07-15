d225d2153b256f663e664145af8103f42951cb72
Cedric Kring <cedric.kring@gmail.com>
2022-07-15 10:06:56 +0200

    refactor(app): remove use of any in App component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
 import "tailwindcss/tailwind.css";
 import "../styles/globals.scss";
-import { Component, FunctionComponent, JSXElementConstructor } from "react";
+import { Component, JSXElementConstructor } from "react";
 import Head from "next/head";
 
-interface Props {
-  Component: JSXElementConstructor<any>;
-  pageProps: unknown;
+interface Props<T> {
+  Component: JSXElementConstructor<T>;
+  pageProps: T;
 }
 
-function App({ Component, pageProps }: Props) {
+function App<T>({ Component, pageProps }: Props<T>) {
   return (
     <>
       <Head>
